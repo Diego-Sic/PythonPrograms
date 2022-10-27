95a1f3c04a025784db983c702138f8759970312b
William Bailey <william.bailey@centre.edu>
2022-10-27 10:54:09 -0400

    Hi Diego. This is good. I've suggested some improvements in comments.

--- Python Project One/DNA WITH DICTIONARY.py
+++ Python Project One/DNA WITH DICTIONARY.py
@@ -1,10 +1,37 @@
 #Diego Sic
 #Project 2 - DNA Identification
 
-from optparse import Values
-from tabnanny import check
-from unicodedata import name
+# WB: You are't using these, so I've commented them out.
+# from optparse import Values
+# from tabnanny import check
+# from unicodedata import name
 
+def strs_to_ints(l_strs):
+    '''Type converts a list of strings to integers. Returns a new list.'''
+    # This is called a list comprehension. It is a compact way of accumulating a new list.
+    return [int(s) for s in l_strs]
+
+# WB: This function for reading a file into a dictionary could be simpler. Try this:
+def breaking_data_base_in_dic_v2(Doc):
+    '''
+    This function will break the lines of a document,
+    and separate the elements using "," as the parameter.
+    Each line will be saved in a list, and all llines will be
+    saved in individual lists.
+    Parameter:
+        A file object, contents formatted as CSV, open in read mode.
+    Return:
+        A dictonary with all the first columns as keys
+        and the values are the rest of the values in the line of the
+        respective key 
+    '''
+    d = {}
+    for line in Doc:
+        line = line.strip().split(",")
+        # Column 1 = name, all others = numbers
+        d[line[0]] = strs_to_ints(line[1:])
+
+    return d
 
 def breaking_data_base_in_dic(Doc):
     '''This function will break the lines of a document,
@@ -25,6 +52,19 @@ def breaking_data_base_in_dic(Doc):
     for elem in List_with_good_elem:
         d[elem[0]] = elem[1:]
     return d
+
+# WB: I think this function is doing too much at once. It would be better if there were a
+# WB: function which counted the number of repetitions of a single STR within a long 
+# WB: sequence, like this: (which can be written using code you already have)
+def STR_repeats_in_sequence(s_STR, s_sequence):
+    '''
+    Paramaters:
+        s_STR [str] a Short Tandem Repeat, typically 4 bases (one of G, T, C, A)
+        s_sqeuence [str] a long sequence of bases to be searched for repeats of s_STR
+    Returns:
+        i_reps [int] the maximum number of uninterrupted repetitions of s_STR in s_sequence
+    '''
+    pass # Avois syntax error with empty defintion
 
 def check_sequence(lines_2, d):
     '''This function will use a dictionary
